d5fcc57c010ed372d1986e956a39020aeef59c56
fulaide <lars.vorreiter@me.com>
2018-05-21 16:16:30 +0800

    Add files via upload

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,10 @@
+// export default {
+//   COSMIC_BUCKET: process.env.COSMIC_BUCKET,
+//   COSMIC_READ_KEY: process.env.COSMIC_READ_KEY,
+//   COSMIC_WRITE_KEY: process.env.COSMIC_WRITE_KEY
+// }
 export default {
-  COSMIC_BUCKET: process.env.COSMIC_BUCKET,
-  COSMIC_READ_KEY: process.env.COSMIC_READ_KEY,
-  COSMIC_WRITE_KEY: process.env.COSMIC_WRITE_KEY
+  COSMIC_BUCKET: 'sparta',
+  COSMIC_READ_KEY: 'LNT2cOwXd3S7cxSdSUrTO4FcB0tS6fxCXbfTwTOST28ZjjZtBk',
+  COSMIC_WRITE_KEY: 'cvczAILIEbhL05XJFgjZLJDWhgHHPhgjt68OR2f9ffqNCM63U5'
 }
